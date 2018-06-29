507120fc27d18d90605fcfcd5d48fa7e2c065cbf
hank <hank.chiu@kaiostech.com>
2018-06-29 09:54:43 +0800

    use pop() when merge

--- js/algorithms/merge.js
+++ js/algorithms/merge.js
@@ -1,29 +1,43 @@
 ((exports) => {
   function sort(arInput){
+    // copy the input
     let ar = arInput.slice(0);
+
+    // done when input size 1
     if(ar.length < 2) return ar;
 
+    // split input to two subarrays 
+    // and recursively sort each subarray
     let left = ar.splice(0, parseInt(ar.length/2));
     let right = ar;
     left = sort(left);
     right = sort(right);
 
-    let i;
-    let iL = 0;
-    let iR = 0;
+    return _merge(left, right);
+  }
+
+  // pop one each time and compare
+  function _merge(left, right) {
     let ret = [];
-    for(i = 0; i < arInput.length; i++){
-      if((left[iL] !== undefined &&
-        left[iL] <= right[iR]) ||
-        right[iR] === undefined){
-        ret.push(left[iL]);
-        iL++;
-      }else{
-        ret.push(right[iR]);
-        iR++;
+    let lo = left.pop();
+    let hi = right.pop();
+    while(lo !== undefined && hi !== undefined) {
+      if (lo === undefined) {
+        ret = ret.concat(hi, right);
+        break;
+      } else if (hi === undefined) {
+        ret = ret.concat(lo, left);
+        break;
+      }
+
+      if (lo <= hi) {
+        ret.push(lo);
+        lo = left.pop();
+      } else {
+        ret.push(hi);
+        hi = right.pop();
       }
     }
-
     return ret;
   }
 
